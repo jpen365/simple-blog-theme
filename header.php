ce8836f44121c1df189ef5912db30ee2f8449f8d
Jon Penland <jon.penland@gmail.com>
2016-09-14 12:09:53 -0600

    Replace static content with WordPress functions

--- header.php
+++ header.php
@@ -10,10 +10,11 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Blog Home - Start Bootstrap Template</title>
+  <link rel="profile" href="http://gmpg.org/xfn/11">
+  <title><?php bloginfo( 'name' ); ?></title>
   <?php wp_head(); ?>
 </head>
 
@@ -30,22 +31,19 @@
                   <span class="icon-bar"></span>
                   <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand" href="#">Start Bootstrap</a>
+              <a class="navbar-brand" href="#"><?php bloginfo( 'name' ); ?></a>
           </div>
           <!-- Collect the nav links, forms, and other content for toggling -->
-          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-              <ul class="nav navbar-nav">
-                  <li>
-                      <a href="#">About</a>
-                  </li>
-                  <li>
-                      <a href="#">Services</a>
-                  </li>
-                  <li>
-                      <a href="#">Contact</a>
-                  </li>
-              </ul>
-          </div>
+          <?php
+            wp_nav_menu( array(
+              'theme_location' => 'primary',
+              'menu_class'     => 'nav navbar-nav',
+              'container_id'   => 'bs-example-navbar-collapse-1',
+              'container_class' => 'collapse navbar-collapse',
+             ));
+          ?>         <!-- /.navbar-collapse -->
+      </div>
+
           <!-- /.navbar-collapse -->
       </div>
       <!-- /.container -->
