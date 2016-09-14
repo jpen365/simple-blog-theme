d6934ea5e6a1defc88ad85749f094d6b3578c09d
Jon Penland <jon.penland@gmail.com>
2016-09-14 16:41:05 -0600

    Replace static content with content loop

--- index.php
+++ index.php
@@ -8,68 +8,20 @@
           <!-- Blog Entries Column -->
           <div class="col-md-8">
 
-              <h1 class="page-header">
-                  Page Heading
-                  <small>Secondary Text</small>
-              </h1>
+              <?php get_template_part( 'template-parts/content' ); ?>
 
-              <!-- First Blog Post -->
-              <h2>
-                  <a href="#">Blog Post Title</a>
-              </h2>
-              <p class="lead">
-                  by <a href="index.php">Start Bootstrap</a>
-              </p>
-              <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
-              <hr>
-              <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-              <hr>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
-              <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-              <hr>
-
-              <!-- Second Blog Post -->
-              <h2>
-                  <a href="#">Blog Post Title</a>
-              </h2>
-              <p class="lead">
-                  by <a href="index.php">Start Bootstrap</a>
-              </p>
-              <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:45 PM</p>
-              <hr>
-              <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-              <hr>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam, quasi, fugiat, asperiores harum voluptatum tenetur a possimus nesciunt quod accusamus saepe tempora ipsam distinctio minima dolorum perferendis labore impedit voluptates!</p>
-              <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-              <hr>
-
-              <!-- Third Blog Post -->
-              <h2>
-                  <a href="#">Blog Post Title</a>
-              </h2>
-              <p class="lead">
-                  by <a href="index.php">Start Bootstrap</a>
-              </p>
-              <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:45 PM</p>
-              <hr>
-              <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-              <hr>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, voluptates, voluptas dolore ipsam cumque quam veniam accusantium laudantium adipisci architecto itaque dicta aperiam maiores provident id incidunt autem. Magni, ratione.</p>
-              <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-              <hr>
-
-              <!-- Pager -->
-              <ul class="pager">
+              <!-- add next/previous navigation to posts page -->
+              <?php if ( is_home() ) {
+                ?><ul class="pager">
                   <li class="previous">
-                      <a href="#">&larr; Older</a>
+                      <?php next_posts_link( '&larr; Older' ); ?>
                   </li>
                   <li class="next">
-                      <a href="#">Newer &rarr;</a>
+                      <?php previous_posts_link( 'Newer &rarr;' ); ?>
                   </li>
-              </ul>
+              </ul><?php
+              } ?>
+              
 
           </div>
 
