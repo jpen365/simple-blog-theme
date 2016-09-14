3cdb7d8ad5f88e370854075182867e65a0cb6273
Jon Penland <jon.penland@gmail.com>
2016-09-14 16:40:37 -0600

    Link site title to home page

--- header.php
+++ header.php
@@ -31,7 +31,7 @@
                   <span class="icon-bar"></span>
                   <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand" href="#"><?php bloginfo( 'name' ); ?></a>
+              <a class="navbar-brand" href="<?php echo get_home_url(); ?>"><?php bloginfo( 'name' ); ?></a>
           </div>
           <!-- Collect the nav links, forms, and other content for toggling -->
           <?php
