a578507bce03dbfece0572f7444e711bf2e5bea6
Jon Penland <jon.penland@gmail.com>
2016-11-15 16:40:22 -0600

    Added categories sidebar widget to functions.php

--- functions.php
+++ functions.php
@@ -141,7 +141,6 @@ add_filter('get_the_excerpt', 'jpen_custom_wp_trim_excerpt');
 
 
 
-
 /* Walker class for comments */
 /* Adapted from GitHub Gist by Georgie Luhur */
 /* Original: https://gist.github.com/georgiecel/9445357 */
@@ -211,5 +210,93 @@ class comment_walker extends Walker_Comment {
   }
 
 
+
+/* Sidebar Categories Widget */
+
+// create category list widget
+class jpen_Category_List_Widget extends WP_Widget {
+
+  // php classnames and widget name/description added
+  function __construct() {
+    $widget_options = array(
+      'classname' => 'jpen_category_list_widget',
+      'description' => 'Add a nicely formatted list of categories to your sidebar.'
+    );
+    parent::__construct( 
+      'jpen_category_list_widget', 
+      'Simple Blog Theme Category List', 
+      $widget_options 
+    );
+  }
+
+
+  // create the widget output
+  function widget( $args, $instance ) {
+    
+    $title = apply_filters( 'widget_title', $instance[ 'title' ] );
+    $categories = get_categories( array(
+      'orderby' => 'name',
+      'order'   => 'ASC'
+      ) );
+    $cat_count = 0;
+    $cat_col_one = [];
+    $cat_col_two = [];
+    foreach( $categories as $category ) {
+      $cat_count ++;
+      $category_link = sprintf( 
+          '<li class="list-unstyled"><a href="%1$s" alt="%2$s">%3$s</a></li>',
+          esc_url( get_category_link( $category->term_id ) ),
+          esc_attr( sprintf( __( 'View all posts in %s', 'textdomain' ), $category->name ) ),
+          esc_html( $category->name )
+      );
+      if ($cat_count % 2 != 0 ) {
+        $cat_col_one[] = $category_link;
+      } else {
+        $cat_col_two[] = $category_link;
+      }
+    }
+
+    echo $args['before_widget'] . $args['before_title'] . $title . $args['after_title'];
+
+    ?><div class="row">
+      <div class="col-lg-6"><?php
+        foreach( $cat_col_one as $cat_one ) {
+          echo $cat_one;
+        } ?>
+      </div>
+
+      <div class="col-lg-6"><?php 
+        foreach( $cat_col_two as $cat_two ) {
+          echo $cat_two;
+        } ?>
+      </div>
+
+    </div><?php
+    echo $args['after_widget'];
+  }
+
+  function form( $instance ) { 
+    $title = ! empty( $instance['title'] ) ? $instance['title'] : ''; ?>
+    <p>
+      <label for="<?php echo $this->get_field_id( 'title' ); ?>">Title:</label>
+      <input type="text" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo esc_attr( $title ); ?>" />
+    </p>
+    <p>This widget displays all of your post categories as a two-column list (or a one-column list when rendered responsively).</p>
+  <?php }
+
+  // Update database with new info
+  function update( $new_instance, $old_instance ) { 
+    $instance = $old_instance;
+    $instance[ 'title' ] = strip_tags( $new_instance[ 'title' ] );
+    return $instance;
+  }
+}
+
+
+// register the widget
+function jpen_register_widgets() { 
+  register_widget( 'jpen_Category_List_Widget' );
+}
+add_action( 'widgets_init', 'jpen_register_widgets' );
 
 ?>
